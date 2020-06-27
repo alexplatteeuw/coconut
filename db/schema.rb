8d44ff596d835d251cdd2b30a28f213bdeef749c
clement lemaire <c.lemaire027@gmail.com>
2020-06-27 16:20:40 +0200

    card ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,69 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_25_190802) do
+ActiveRecord::Schema.define(version: 2020_06_25_193140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "associations", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "project_id", null: false
+    t.date "begin_date"
+    t.date "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_bookings_on_project_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "address"
+    t.string "name"
+    t.text "description"
+    t.bigint "association_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["association_id"], name: "index_projects_on_association_id"
+  end
+
+  create_table "projects_skills", force: :cascade do |t|
+    t.bigint "project_id", null: false
+    t.bigint "skill_id", null: false
+    t.integer "duration_in_hours"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_projects_skills_on_project_id"
+    t.index ["skill_id"], name: "index_projects_skills_on_skill_id"
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_skills_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +86,10 @@ ActiveRecord::Schema.define(version: 2020_06_25_190802) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "projects"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "projects", "associations"
+  add_foreign_key "projects_skills", "projects"
+  add_foreign_key "projects_skills", "skills"
+  add_foreign_key "skills", "users"
 end
