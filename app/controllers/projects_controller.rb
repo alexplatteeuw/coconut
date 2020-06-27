4dc9b102b3dd946b282c2742a79caaae914b40b9
bart boillot <barthboillot@hotmail.com>
2020-06-27 17:07:37 +0200

    add index and show

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,10 @@
 class ProjectsController < ApplicationController
   def index
-    @projects = policy_scope(Project).order(created_at: :desc)
+    if params[:q].present?
+      @projects = policy_scope(Project).where("name ILIKE ?", "%#{params[:q]}%")
+    else
+      @projects = policy_scope(Project).order(created_at: :desc)
+    end
   end
 
   def show
