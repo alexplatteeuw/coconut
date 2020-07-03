f1cd25d6779e23a73cf1b00a05dcd95eaf72c5cd
Damien Milon <milon.damien@gmail.com>
2020-07-03 14:20:37 +0200

    remove chartkick

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,12 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("chartkick")
-require("chart.js")
-require("chartkick").use(require("highcharts"))
-console.log("hello")
-
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -36,5 +30,4 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-
 });
