1ecee6224783b08f6c4f12fc296a190809ed147b
Alex Platteeuw <aplatteeuw@outlook.fr>
2020-07-04 16:33:19 +0200

    avatar added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.crea
-puts 'destroy charity db'
-Charity.destroy_all
+puts 'Cleaning DB ...'
 
-puts 'Creating 2 new charitys'
+Charity.destroy_all
+Project.destroy_all
+Company.destroy_all
+User.destroy_all
+
+puts 'Creating 2 charities ...'
+
 charity = Charity.new
 charity.name =  "les Restos du Coeur"
 charity.description = "Fondés par Coluche en 1985, les Restos du Cœur est une association loi de 1901, reconnue d’utilité publique, sous le nom officiel de « les Restaurants du Cœur – les Relais du Cœur ». Ils ont pour but « d’aider et d’apporter une assistance bénévole aux personnes démunies, notamment dans le domaine alimentaire par l’accès à des repas gratuits, et par la participation à leur insertion sociale et économique, ainsi qu’à toute action contre la pauvreté sous toutes ses formes »."
@@ -16,13 +14,13 @@ charity.save!
 
 charity = Charity.new
 charity.name = "Médecins sans frontières"
-charity.description = "epuis plus de 45 ans, Médecins Sans Frontières apporte une assistance médicale à des populations dont la vie ou la santé est menacée par des conflits armés, des épidémies, des pandémies ou des catastrophes naturelles."
+charity.description = "Depuis plus de 45 ans, Médecins Sans Frontières apporte une assistance médicale à des populations dont la vie ou la santé est menacée par des conflits armés, des épidémies, des pandémies ou des catastrophes naturelles."
 charity.save!
 
-puts 'destroy projects db'
-Project.destroy_all
+puts 'Charities created!'
 
-puts 'Creating 3 new projects'
+puts 'Creating 3 projects belonging to the same charity...'
+
 project = Project.new
 project.name = "Site internet"
 project.description ="Je veux refaire mon site"
@@ -50,19 +48,17 @@ project.tag_list = "informatique, développement, long, intranet"
 project.skill_list = "Python, React, JS, CSS, Ruby"
 project.save!
 
-puts "Drop company db"
-Company.destroy_all
+puts 'Projects created!'
 
-puts "Add a company"
+puts 'Creating a company...'
+
 company = Company.new
 company.name = "Le Wagon"
 company.address = "16 villa Gaudelet"
 company.save!
 
-puts "Drop users db"
-User.destroy_all
+puts 'Creating 2 users belonging to the same company...'
 
-puts "Add 2 users"
 admin_user = User.new
 admin_user.first_name = "Maxime"
 admin_user.last_name = "Berthelot"
@@ -73,6 +69,7 @@ admin_user.admin = true
 admin_user.company = Company.first
 admin_user.address = "31 rue de citeaux, 75012"
 admin_user.skill_list ="CSS, JS, Deug"
+admin_user.avatar_url = "https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1581162961/elniwt7txfiooiujd3ew.jpg"
 admin_user.save!
 
 user = User.new
@@ -85,4 +82,7 @@ user.admin = false
 user.address = "25 rue du javelot, 75013"
 user.skill_list = "CSS, JS, Setup"
 user.company = Company.first
+user.avatar_url = "https://avatars3.githubusercontent.com/u/60347959?v=4"
 user.save!
+
+puts 'Users created!'
