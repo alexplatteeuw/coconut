d569e1abf1df9b965b8985df78277fd2a96b3d2b
Alex Platteeuw <aplatteeuw@outlook.fr>
2020-07-10 20:40:52 +0200

    set algoriasearch config

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,10 +2,16 @@ class Project < ApplicationRecord
   belongs_to :charity
   has_many :reservations
   has_many :users, -> { distinct }, through: :reservations
-  acts_as_taggable_on :tags, :skills
 
   validates :name, :address, :description, presence: true
   validates :status, inclusion: { in: ['unstarted', 'current', 'completed'] }, presence: true
 
+  acts_as_taggable_on :tags, :skills
   acts_as_favoritable
+
+  include AlgoliaSearch
+
+  algoliasearch do
+    attributes :name, :description
+  end
 end
